ef611f827fea74eba2792e45c19fc4a30e3dfcf3
Max <max@autistici.org>
2018-06-21 14:26:46 +0100

    web server

---
+++ main.py
@@ -0,0 +1,15 @@
+### MAIN ###
+import signal
+import sys
+from WebServer import WebServer
+def shutdownServer(sig, unused):
+    """
+    Shutsdown server from a SIGINT recieved signal
+    """
+    server.shutdown()
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, shutdownServer)
+server = WebServer(8080)
+server.start()
+print("Press Ctrl+C to shut down server.")
